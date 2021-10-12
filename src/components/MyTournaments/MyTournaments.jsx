b38eb879597f91505f1619f44ace87833dccee4b
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-10-12 14:23:24 -0500

    Added my tournaments base components

---
+++ src/components/MyTournaments/MyTournaments.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+function MyTournaments(){
+    const store = useSelector( store => store )
+    
+    return(
+        <>
+        </>
+    )
+}
+
+export default MyTournaments;
