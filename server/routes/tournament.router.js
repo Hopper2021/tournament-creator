d2de8c8ce8b4bdab6aff87a0e19a4a73c140f67b
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-10-12 14:42:48 -0500

    Added GET all users tournaments in tournament router

--- server/routes/tournament.router.js
+++ server/routes/tournament.router.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+    rejectUnauthenticated,
+  } = require('../modules/authentication-middleware');
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-  // GET route code here
+router.get('/', rejectUnauthenticated, (req, res) => {
+    console.log('User - ', req.user);
+    
+    const sqlText = `SELECT * FROM "tournament" WHERE "user_id" = $1`
+    pool.query(sqlText, [req.user])
+    .then((results) => {
+        res.send(results.rows)})
+    .catch((error) => {
+        console.log('Error in GET all user\'s tournaments', error);
+        res.sendStatus(500);
+    });
 });
 
 /**
