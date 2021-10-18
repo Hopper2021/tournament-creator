35c66d522bc6ab1a9b07c52fd96ca758cd311e20
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-10-17 19:22:17 -0500

    Attempted to add counter as new entrant id

--- src/components/CreateEntrants/CreateEntrants.jsx
+++ src/components/CreateEntrants/CreateEntrants.jsx
@@ -11,7 +11,6 @@ function CreateTournamentEntrants() {
     const store = useSelector(store => store);
     const kingdoms = store.kingdoms;
     const entrants = store.entrants;
-    const kingdom_id = store.entrants.kingdom_id;
     const [counter, setCounter] = useState(1);
 
     useEffect(() => {
@@ -20,98 +19,91 @@ function CreateTournamentEntrants() {
     }, [])
 
     const [newEntrant, setNewEntrant] = useState({
-        persona: '', kingdom_id: '', warriors: ''
+        id: '', persona: '', kingdom_id: '', warriors: '', score: ''
     });
 
     const addEntrant = (event) => {
         event.preventDefault();
-        setCounter(counter + 1); // displays entrant number, which is one more than the index
+        const index = setCounter(counter + 1); // displays entrant number, which is one more than the index
+        setNewEntrant({...newEntrant, id: index})
         console.log(newEntrant); 
         dispatch({ type: 'ADD_ENTRANT', payload: newEntrant })
         setNewEntrant({
-            persona: '', kingdom_id: '', warriors: ''
+            id: '', persona: '', kingdom_id: '', warriors: '', score: ''
         })
     }
 
     const createTournament = () => {
         history.push('/create/scores');
     }
-    
-    // TODO Trying to show kingdom name to table instead of kingdom id
-    const displayKingdomName = (kingdom_id) => {
-        let result;
-        for ( let i=0; i<kingdoms.length; i++) {
-            if ( kingdoms[i].id == kingdom_id ) {
-                let result = kingdoms[i].name;
-            }
-            // for ( let i=0; i<entrants.length; i++) {
-            //     if ( kingdoms[i].id == entrants[i].kingdom_id ) {
-            //         return result = kingdoms[i].name;
-            //     }
-            // }
-        }
-        return result;
-        console.log('Result - ', result);
-    }
 
     return (
         <div className="container">
         {JSON.stringify(store.tournaments.newTournament)}
         {JSON.stringify(store.entrants)}
-        <h1 className="create-tournament-header">
-            {store.tournaments.newTournament.name}
-        </h1>
-        <h2 className="create-tournament-header">Entrant # {counter}</h2>
-        <form className="create-tournament-form"
+
+            <h1 className="create-tournament-header">
+                {store.tournaments.newTournament.name}
+            </h1>
+            <h2 className="create-tournament-header" >
+                Entrant # {counter}
+            </h2>
+            <form 
+            className="create-tournament-form"
             onSubmit={addEntrant}>
-            <input required type="text" className="create-tournament-input"
-                value={newEntrant.persona}
-                placeholder="Persona"
-                onChange={(event) => setNewEntrant({...newEntrant, persona: event.target.value})}
-            />
-            <select required className="create-tournament-select"
-                value={newEntrant.kingdom_id}
-                onChange={(event) => setNewEntrant({...newEntrant, kingdom_id: event.target.value})}>
-                <option value="" disabled selected>Kingdom</option>
-                    {kingdoms.map((kingdom) => (
-                        <option value={kingdom.id}>{kingdom.name}</option>
-                    ))}
-            </select>
-            <input type="number" className="create-tournament-input"
-                placeholder="Orders of the Warrior"
-                value={newEntrant.warriors}
-                onChange={(event) => setNewEntrant({...newEntrant, warriors: event.target.value})}
-            />
-            <br />
-            <Button 
-                sx={{bgcolor: red[900]}}
-                variant="contained"
-                type="submit">
-                    Add Entrant
+                <input required 
+                    type="text" 
+                    className="create-tournament-input"
+                    value={newEntrant.persona}
+                    placeholder="Persona"
+                    onChange={(event) => setNewEntrant({...newEntrant, persona: event.target.value})}
+                />
+                <select required 
+                    className="create-tournament-select"
+                    value={newEntrant.kingdom_id}
+                    onChange={(event) => setNewEntrant({...newEntrant, kingdom_id: event.target.value})}>
+                        <option value="" disabled selected>Kingdom</option>
+                        {kingdoms.map((kingdom) => (
+                            <option value={kingdom.id}>{kingdom.name}</option>
+                        ))}
+                </select>
+                <input 
+                    type="number" 
+                    className="create-tournament-input"
+                    placeholder="Orders of the Warrior"
+                    value={newEntrant.warriors}
+                    onChange={(event) => setNewEntrant({...newEntrant, warriors: event.target.value})}
+                />
+                <br />
+                <Button 
+                    sx={{ bgcolor: red[900] }}
+                    variant="contained"
+                    type="submit">
+                        Add Entrant
+                </Button>
+            </form>
+            <table id="entrants">
+                <tr>
+                    <th>#</th>
+                    <th>Persona</th>
+                    <th>Kingdom</th>
+                    <th>Warriors</th>
+                </tr>
+                {entrants.map((entrant, index) => (
+                <tr key={entrant.id}>
+                    <td>{index + 1}</td>
+                    <td>{entrant.persona}</td>
+                    <DisplayKingdomName entrant={entrant} />
+                    <td>{entrant.warriors}</td>
+                </tr>
+                ))}
+            </table>
+            <Button onClick={createTournament}
+                sx={{ bgcolor: red[900] }}
+                id="create-tournament-button"
+                variant="contained">
+                    Start Tournament
             </Button>
-        </form>
-        <table id="entrants">
-            <tr>
-                <th>#</th>
-                <th>Persona</th>
-                <th>Kingdom</th>
-                <th>Warriors</th>
-            </tr>
-            {entrants.map((entrant, index) => (
-            <tr key={entrant.id}>
-                <td>{index + 1}</td>
-                <td>{entrant.persona}</td>
-                <DisplayKingdomName entrant={entrant} />
-                <td>{entrant.warriors}</td>
-            </tr>
-            ))}
-        </table>
-        <Button onClick={createTournament}
-            sx={{bgcolor: red[900]}}
-            id="create-tournament-button"
-            variant="contained">
-                Start Tournament
-        </Button>
         </div>
     )
 }
--- src/components/CreateScoresItem/CreateScoresItem.jsx
+++ src/components/CreateScoresItem/CreateScoresItem.jsx
@@ -1,19 +1,30 @@
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import DoDisturbOnIcon from '@mui/icons-material/DoDisturbOn';
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function CreateScoresItem({ entrant, index }) {
-    const [score, setScore] = useState(0);
+    const dispatch = useDispatch();
+    const store = useSelector(store => store)
+    const [newScore, setNewScore] = useState(0);
     const [streak, setStreak] = useState(0);
     const [streakArray, setStreakArray] = useState([]);
+    const [updateEntrant, setUpdateEntrant] = useState(entrant)
 
-    const handleMinus = () => {
-        setScore(score - 1);
+    const handleMinus = (event) => { 
+        // console.log('Before update - ', entrant);
+        const score = setNewScore(event.target.value); 
+        setUpdateEntrant({...updateEntrant, score: score})
+        console.log('After update - ', entrant);
+        // dispatch({ type: 'SET_SCORE', payload: entrant })
     }
 
-    const handlePlus = (index) => {
-        setScore(score + 1);
-        console.log('New Score - ', score);
+    const handlePlus = (event) => {
+        // console.log('Before update - ', entrant);
+        setNewScore(newScore + 1);
+        setUpdateEntrant({...updateEntrant, score: newScore})
+        console.log('After update - ', entrant);
+        // dispatch({ type: 'SET_SCORE', payload: entrant })
         // To track streak, check if the index is the same as the last button pressed
         // If it is, update current streak state
         // If it is not, add last number to streakArray
@@ -44,12 +55,15 @@ function CreateScoresItem({ entrant, index }) {
 
     return(
         <tr>
-            <td>{index + 1}</td>
-            <td>{entrant.persona}</td>
-            <td className="score-table-data">
-            <DoDisturbOnIcon onClick={() => handleMinus(index)}/>
-                <span className="score">{score}</span>
-            <AddCircleIcon onClick={() => handlePlus(index)}/>
+            {JSON.stringify(entrant.score)}
+            <td id="index-table-data">{index + 1}</td>
+            <td id="persona-table-data">{entrant.persona}</td>
+            <td id="score-table-data">
+            <DoDisturbOnIcon value={newScore} onClick={(event) => handleMinus(event.target.value)}
+                sx={{ fontSize: 50 }}/>
+                <span id="score-counter">{newScore}</span>
+            <AddCircleIcon value={newScore} onClick={(event) => handlePlus(event.target.vaule)}
+                sx={{ fontSize: 50 }}/>
             </td>
             {/* <td>{streak}</td> */}
         </tr>
