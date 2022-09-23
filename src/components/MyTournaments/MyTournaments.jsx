a7ad088a5757ee3ec1a3e781801e209209e26daf
Jessica.Buckwalter <jessica.buckwalter@target.com>
2022-09-23 13:36:16 -0500

    Info message if no tournaments have been made yet

--- src/components/MyTournaments/MyTournaments.jsx
+++ src/components/MyTournaments/MyTournaments.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import MyTournamentsItem from '../MyTournamentsItem/MyTournamentsItem';
+// import { classNames } from 'MyTournaments.css';
 
 function MyTournaments(){
     const tournaments = useSelector(store => store.tournaments.tournamentReducer);
@@ -13,12 +14,17 @@ function MyTournaments(){
 
     return (
         <div>
-            {/* {JSON.stringify(tournaments)} */}
             {tournaments.map(tournament => (
             <div key={tournament.id} id="my-tournament-container">
                 <MyTournamentsItem key={tournament.id} tournament={tournament}/>
             </div>
             ))}
+            {console.log(tournaments)}
+            <div>
+                {tournaments <= 0 && (
+                    <div>No tournaments to display.</div>
+                )}
+            </div>
         </div>
     )
 }
