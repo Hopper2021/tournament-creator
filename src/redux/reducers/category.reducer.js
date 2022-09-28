53aae55be1646318be2e8ec04007ff5849cb5d06
Jessica.Buckwalter <jessica.buckwalter@target.com>
2022-09-27 21:31:53 -0500

    Create category reducer

---
+++ src/redux/reducers/category.reducer.js
@@ -0,0 +1,10 @@
+const categoryReducer = (state = [], action) => {
+    switch ( action.type ) {
+        case 'SET_CATEGORIES':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+export default categoryReducer;
